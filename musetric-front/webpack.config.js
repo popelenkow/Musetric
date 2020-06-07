bf0be768eb53414e40d5a9422488845dc70e44e7
Popelenkov Vladlen <vladlen.popelenkov@grapecity.com>
2020-06-07 21:06:38 +0700

    Add webpack & electron & typescript & react

--- musetric-front/webpack.config.js
+++ musetric-front/webpack.config.js
@@ -1,28 +1,24 @@
-
-const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-  entry: './src/index.tsx',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js']
-  },
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.min.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader'
-      }
+ 
+module.exports = [
+  {
+    mode: 'development',
+    entry: './src/react.tsx',
+    target: 'electron-renderer',
+    devtool: 'source-map',
+    module: { rules: [{
+      test: /\.ts(x?)$/,
+      include: /src/,
+      use: [{ loader: 'ts-loader' }]
+    }] },
+    output: {
+      path: __dirname + '/dist',
+      filename: 'react.js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
-}
+  }
+];
