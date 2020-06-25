c1e5fb57fd1599eaeb8cbbfd9a52a3156b3942f7
Popelenkov Vladlen <vladlen.popelenkov@grapecity.com>
2020-06-26 00:46:55 +0700

    Use icons

--- musetric-front/src/components/Titlebar/view.tsx
+++ musetric-front/src/components/Titlebar/view.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import i18n from 'i18next'
 import { TitlebarProps, TitlebarState } from './types';
 import { remote } from 'electron'
+import { icons } from '../../icons';
 
 const win = remote.getCurrentWindow();
 
@@ -17,11 +18,11 @@ export class TitlebarView extends React.Component<TitlebarProps, TitlebarState> 
 		const isMaximized = this.state.isMaximized;
 		const maximizeBtn = (
 		<button className="title-btn maximize" onClick={() => { win.maximize(); this.setState({ isMaximized: !isMaximized }) }}>
-			<svg viewBox="0 0 10 10"><path d="M0,0v10h10V0H0z M9,9H1V1h8V9z" /></svg>
+			{icons.titlebar.maximize}
 		</button>);
 		const unmaximizeBtn = (
 		<button className="title-btn unmaximize" onClick={() =>  { win.unmaximize(); this.setState({ isMaximized: !isMaximized }) }}>
-			<svg viewBox="0 0 10.2 10.1"><path d="M2.1,0v2H0v8.1h8.2v-2h2V0H2.1z M7.2,9.2H1.1V3h6.1V9.2z M9.2,7.1h-1V2H3.1V1h6.1V7.1z" /></svg>
+			{icons.titlebar.unmaximize}
 		</button>);
 
 		return (
@@ -29,11 +30,11 @@ export class TitlebarView extends React.Component<TitlebarProps, TitlebarState> 
 			<div className="title-text">Musetric</div>
 			<div className="title-controls">
 				<button className="title-btn minimize" onClick={() => win.minimize()}>
-					<svg x="0px" y="0px" viewBox="0 0 10.2 1"><rect x="0" y="50%" width="10.2" height="1" /></svg>
+					{icons.titlebar.minimize}
 				</button>
 				{isMaximized ? unmaximizeBtn : maximizeBtn} 
 				<button className="title-btn close" onClick={() => win.close()}>
-					<svg viewBox="0 0 10 10"><polygon points="10.2,0.7 9.5,0 5.1,4.4 0.7,0 0,0.7 4.4,5.1 0,9.5 0.7,10.2 5.1,5.8 9.5,10.2 10.2,9.5 5.8,5.1" /></svg>
+					{icons.titlebar.close}
 				</button>
 			</div>
 		</div>)
---
+++ musetric-front/src/icons.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const icons = {
+	titlebar: {
+		minimize: <svg x="0px" y="0px" viewBox="0 0 10.2 1"><rect x="0" y="50%" width="10.2" height="1" /></svg>,
+		maximize: <svg viewBox="0 0 10 10"><path d="M0,0v10h10V0H0z M9,9H1V1h8V9z" /></svg>,
+		unmaximize: <svg viewBox="0 0 10.2 10.1"><path d="M2.1,0v2H0v8.1h8.2v-2h2V0H2.1z M7.2,9.2H1.1V3h6.1V9.2z M9.2,7.1h-1V2H3.1V1h6.1V7.1z" /></svg>,
+		close: <svg viewBox="0 0 10 10"><polygon points="10.2,0.7 9.5,0 5.1,4.4 0.7,0 0,0.7 4.4,5.1 0,9.5 0.7,10.2 5.1,5.8 9.5,10.2 10.2,9.5 5.8,5.1" /></svg>,
+	}
+}
