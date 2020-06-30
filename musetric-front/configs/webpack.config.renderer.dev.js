c21b61d1071bbb32036e7c3a66a9593f66029b6c
Popelenkov Vladlen <vladlen.popelenkov@grapecity.com>
2020-06-30 17:05:03 +0700

    Refactor dev webpack

--- musetric-front/configs/webpack.config.renderer.dev.js
+++ musetric-front/configs/webpack.config.renderer.dev.js
@@ -3,24 +3,25 @@ const { spawn } = require('child_process');
 const merge = require('webpack-merge');
 const config = require('./webpack.config.renderer');
 
+const runOnDoneOnce = (compiler, run) => {
+	let done = false
+	compiler.hooks.done.tap('run-main', () => {
+		!done && run()
+		done = true; 
+	})
+}
+
 module.exports = merge(config, {
 	mode: "development",
 	devtool: 'source-map',
 	devServer: {
 		contentBase: path.join(__dirname, 'dist'),
 		port: 8080,
-		before: (app, server, compiler) => {
-			let done = false;
-			compiler.hooks.done.tap('main', () => {
-				if (done) return;
-				done = true;
-				console.log('Starting Main Process...');
-				spawn('yarn', ['dev-main'], { shell: true, env: process.env, stdio: 'inherit' })	
-					.on('close', code => process.exit(code))
-					.on('error', spawnError => console.error(spawnError));
-				compiler.hooks.done.rem
-			})
-			
-		}
+		before: (_app, _server, compiler) => process.env.WITH_MAIN && runOnDoneOnce(compiler, () => {
+			console.log('Starting Main Process...');
+			spawn('yarn', ['dev-main'], { shell: true, env: process.env, stdio: 'inherit' })	
+				.on('close', code => process.exit(code))
+				.on('error', spawnError => console.error(spawnError));
+		})
 	}
 });
