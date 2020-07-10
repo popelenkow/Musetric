0dad345e38303812ac4bd0985d455822c69e28d9
Popelenkov Vladlen <popelenkow@gmail.com>
2020-07-11 00:30:40 +0700

    Refactor

--- desktop-app/src/main.ts
+++ desktop-app/src/main.ts
@@ -1,12 +1,29 @@
-import { app, BrowserWindow, Menu, MenuItemConstructorOptions, ipcMain } from 'electron'
+import { app, BrowserWindow, ipcMain, globalShortcut } from 'electron'
 import { PythonShell } from 'python-shell'
 import { channels, WindowEvent } from './channels'
 
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 const isDev = process.env.NODE_ENV === 'development'
 
-app.on('ready', () => {
-	let window = new BrowserWindow({
+app.whenReady().then(() => {
+	ipcMain.handle(channels.mainWindow, async (electronEvent, event: WindowEvent) => {
+		const window = BrowserWindow.fromId(electronEvent.sender.id);
+		if (event == 'close') isDev ? window.destroy() : window.close()
+		else if (event == 'minimize') window.minimize()
+		else if (event == 'maximize') window.maximize()
+		else if (event == 'unmaximize') window.unmaximize()
+	})
+	
+	ipcMain.handle(channels.pytest, async () => {
+		return await new Promise((resolve) => {
+			PythonShell.run('background/hello.py', undefined, (err, results) =>  {
+				resolve({ message: 'python complete', results, err })
+			});
+		})
+		
+	})
+
+	const window = new BrowserWindow({
 		width: 1200,
 		height: 600,
 		minWidth: 300,
@@ -19,69 +36,17 @@ app.on('ready', () => {
 
 	isDev ? window.loadURL('http://localhost:8080') : window.loadFile('dist/index.html');
 
-	isDev && window.setIcon('resources/icon.ico')
+	globalShortcut.register('Ctrl+Q', () => isDev ? window.destroy() : window.close())
+	isDev && globalShortcut.register('F12', () => window.webContents.toggleDevTools())
 	isDev && window.webContents.toggleDevTools();
+	isDev && window.setIcon('resources/icon.ico')
+	
 	
 	window.on('maximize', () => window.webContents.send(channels.onMaximizeWindow, true))
 	window.on('unmaximize', () => window.webContents.send(channels.onMaximizeWindow, false))
 	window.on('closed', () => app.quit())
-	Menu.setApplicationMenu(Menu.buildFromTemplate(mainMenuTemplate))
-});
-
-const mainMenuTemplate: Array<MenuItemConstructorOptions> = [{
-	label: 'File',
-	submenu: [
-	{
-		label: 'Add Item'
-	},
-	{
-		label: 'Clear Items'
-	},
-	{
-		label: 'Quit',
-		accelerator: 'Ctrl+Q',
-		click() {
-			app.quit();
-		}
-	}
-	]
-}]
-
-ipcMain.handle(channels.mainWindow, async (electronEvent, event: WindowEvent) => {
-	const window = BrowserWindow.fromId(electronEvent.sender.id);
-	if (event == 'close') isDev ? window.destroy() : window.close()
-	else if (event == 'minimize') window.minimize()
-	else if (event == 'maximize') window.maximize()
-	else if (event == 'unmaximize') window.unmaximize()
-})
-
-ipcMain.handle(channels.pytest, async () => {
-	return await new Promise((resolve) => {
-		PythonShell.run('background/hello.py', undefined, (err, results) =>  {
-			resolve({ message: 'python complete', results, err })
-		});
-	})
-	
 })
 
 
-if (isDev) {
-	mainMenuTemplate.push({
-		label: 'Developer Tools',
-		submenu: [
-		{
-			label: 'Toggle DevTools',
-			accelerator: 'F12',
-			click(item, focusedWindow) {
-				if (focusedWindow) {
-					focusedWindow.webContents.toggleDevTools();
-				}
-			}
-		},
-		{
-			role: 'reload',
-			accelerator: 'F5'
-		}
-		]
-	})
-}
+
+
