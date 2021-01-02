8cfdbe9b60b0cb88be726f95360d9aea8712cb5b
Popelenkov Vladlen <popelenkow@gmail.com>
2021-01-03 01:40:09 +0700

    Move elements to components folder

--- musetric/src/index.ts
+++ musetric/src/index.ts
@@ -1,5 +1,4 @@
 export * as Components from './components';
 export * as Contexts from './contexts';
-export * as Controls from './controls';
 export * as Locales from './locales';
 export * as Types from './types';
