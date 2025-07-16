274e44de43ab77a82e7d927a8da7f6b54886728f
Vladlen Popelenkov <popelenkow@gmail.com>
2025-07-17 02:11:32 +0700

    Use single complex gpu buffer in-out for radix 4 (#158)

--- packages/audio-view/src/fourier/gpuFftRadix4/bindGroup.ts
+++ packages/audio-view/src/fourier/gpuFftRadix4/bindGroup.ts
@@ -9,12 +9,10 @@ export const createReverseBindGroup = (
     label: 'fft4-reverse-bind-group',
     layout: pipeline.getBindGroupLayout(0),
     entries: [
-      { binding: 0, resource: { buffer: buffers.inputReal } },
-      { binding: 1, resource: { buffer: buffers.inputImag } },
-      { binding: 2, resource: { buffer: buffers.outputReal } },
-      { binding: 3, resource: { buffer: buffers.outputImag } },
-      { binding: 4, resource: { buffer: buffers.reverseTable } },
-      { binding: 5, resource: { buffer: buffers.params } },
+      { binding: 0, resource: { buffer: buffers.dataReal } },
+      { binding: 1, resource: { buffer: buffers.dataImag } },
+      { binding: 2, resource: { buffer: buffers.reverseTable } },
+      { binding: 3, resource: { buffer: buffers.params } },
     ],
   });
 
@@ -27,8 +25,8 @@ export const createTransformBindGroup = (
     label: 'fft4-transform-bind-group',
     layout: pipeline.getBindGroupLayout(0),
     entries: [
-      { binding: 0, resource: { buffer: buffers.outputReal } },
-      { binding: 1, resource: { buffer: buffers.outputImag } },
+      { binding: 0, resource: { buffer: buffers.dataReal } },
+      { binding: 1, resource: { buffer: buffers.dataImag } },
       { binding: 2, resource: { buffer: buffers.trigTable } },
       { binding: 3, resource: { buffer: buffers.params } },
     ],
