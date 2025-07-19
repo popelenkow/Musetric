e37229cc50c49f5c3dfa965078705bfd98f321af
Vladlen Popelenkov <popelenkow@gmail.com>
2025-07-19 13:52:47 +0700

    Perf

--- packages/audio-view/src/spectrogram/cpu/pipelineTimer.ts
+++ packages/audio-view/src/spectrogram/cpu/pipelineTimer.ts
@@ -1,31 +1,38 @@
 import { createCpuTimer } from '../../common';
 
-const create = () =>
-  createCpuTimer([
-    'fourier',
-    'normalizeMagnitude',
-    'normalizeDecibel',
-    'scaleView',
-    'draw',
-    'resize',
-    'sliceWaves',
-    'total',
-  ]);
+export const cpuMetricKeys = [
+  'resize',
+  'sliceWaves',
+  'fourier',
+  'normalizeMagnitude',
+  'normalizeDecibel',
+  'scaleView',
+  'draw',
+  'total',
+] as const;
+
+const create = () => createCpuTimer(cpuMetricKeys);
 
 type Timer = ReturnType<typeof create>;
+
+export type PipelineProfile = ReturnType<Timer['read']>;
 
 export type PipelineTimer = {
   wrap: Timer['wrap'];
   wrapAsync: Timer['wrapAsync'];
-  read?: () => ReturnType<Timer['read']>;
+  finish: () => void;
 };
 
-export const createPipelineTimer = (profiling?: boolean): PipelineTimer => {
-  if (!profiling) {
+export const createPipelineTimer = (
+  onProfile?: (profile: PipelineProfile) => void,
+): PipelineTimer => {
+  if (!onProfile) {
     return {
       wrap: (_label, fn) => fn,
       wrapAsync: (_label, fn) => fn,
-      read: undefined,
+      finish: async () => {
+        /** Nothing */
+      },
     };
   }
 
@@ -34,6 +41,9 @@ export const createPipelineTimer = (profiling?: boolean): PipelineTimer => {
   return {
     wrap: timer.wrap,
     wrapAsync: timer.wrapAsync,
-    read: timer.read,
+    finish: async () => {
+      const profile = timer.read();
+      onProfile(profile);
+    },
   };
 };
