94d776a3bfdea95298addd559e0c8f28b889fd0a
Vladlen Popelenkov <popelenkow@gmail.com>
2025-07-28 00:34:37 +0700

    Enhance waveform to use pixel-grouped bar segments (#192)

---
+++ packages/audio-view/src/waveform/draw.ts
@@ -0,0 +1,50 @@
+import { Colors } from './colors';
+import { WaveSegment } from './generateSegments';
+
+export type Draw = {
+  run: (segments: WaveSegment[], progress: number, colors: Colors) => void;
+};
+export const createDraw = (canvas: HTMLCanvasElement): Draw => {
+  const context = canvas.getContext('2d');
+  if (!context) {
+    throw new Error('Context 2D not available on the canvas');
+  }
+
+  const ref: Draw = {
+    run: (segments, progress, colors) => {
+      const width = canvas.clientWidth;
+      const height = canvas.height;
+      canvas.width = width;
+      canvas.height = height;
+
+      context.clearRect(0, 0, width, height);
+
+      const clampedProgress = Math.max(0, Math.min(progress, 1));
+      const playedCount = Math.floor(clampedProgress * segments.length);
+
+      const segmentWidth = width / segments.length;
+      const barWidth = segmentWidth * 0.9;
+
+      context.fillStyle = colors.unplayed;
+      for (let i = 0; i < segments.length; i++) {
+        const { min, max } = segments[i];
+        const x = i * segmentWidth;
+        const yStart = height * ((1 - max) / 2);
+        const yEnd = height * ((1 - min) / 2);
+        const barHeight = yEnd - yStart;
+        context.fillRect(x, yStart, barWidth, barHeight);
+      }
+
+      context.fillStyle = colors.played;
+      for (let i = 0; i < playedCount; i++) {
+        const { min, max } = segments[i];
+        const x = i * segmentWidth;
+        const yStart = height * ((1 - max) / 2);
+        const yEnd = height * ((1 - min) / 2);
+        const barHeight = yEnd - yStart;
+        context.fillRect(x, yStart, barWidth, barHeight);
+      }
+    },
+  };
+  return ref;
+};
