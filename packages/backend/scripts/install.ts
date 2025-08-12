82b12e19fed1e0c8c6a74689314232a194a11d16
Vladlen Popelenkov <popelenkow@gmail.com>
2025-08-13 02:20:09 +0700

    Add workspace_mode env and strict yarn

--- packages/backend/scripts/install.ts
+++ packages/backend/scripts/install.ts
@@ -1,9 +1,9 @@
 import { execSync } from 'child_process';
 
-if (process.env.CI_CHECK) {
+if (process.env.workspace_mode === 'check') {
   console.log('▶ Generating Prisma client…');
-  execSync('npx prisma generate', { stdio: 'inherit' });
+  execSync('yarn prisma generate', { stdio: 'inherit' });
 } else {
   console.log('▶ Setting up database…');
-  execSync('npx prisma db push', { stdio: 'inherit' });
+  execSync('yarn prisma db push', { stdio: 'inherit' });
 }
