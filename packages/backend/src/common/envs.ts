1e35d6466ebbb424d6c868a8cc7365d7dd61ed4f
Vladlen Popelenkov <popelenkow@gmail.com>
2025-08-13 02:20:09 +0700

    Move envs to common folder

--- packages/backend/src/common/envs.ts
+++ packages/backend/src/common/envs.ts
@@ -1,7 +1,7 @@
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-const currentDir = dirname(fileURLToPath(import.meta.url));
+const rootPath = join(dirname(fileURLToPath(import.meta.url)), '../../');
 
 export const envs = {
   version: process.env.VERSION ?? '0.1.0',
@@ -9,6 +9,6 @@ export const envs = {
   port: process.env.PORT ? Number(process.env.PORT) : 3000,
   logLevel: process.env.LOG_LEVEL ?? 'info',
   protocol: process.env.PROTOCOL === 'http' ? 'http' : 'https',
-  blobsPath: join(currentDir, process.env.BLOBS_PATH ?? '../tmp/blobs'),
-  publicPath: join(currentDir, '../public'),
+  blobsPath: join(rootPath, process.env.BLOBS_PATH ?? 'tmp/blobs'),
+  publicPath: join(rootPath, 'public'),
 };
