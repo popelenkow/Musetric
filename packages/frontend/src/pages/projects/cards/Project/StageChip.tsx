9bb90b423a80326d0c80b52f563cd04c9bb62eb3
Vladlen Popelenkov <popelenkow@gmail.com>
2025-08-13 02:20:09 +0700

    Add stage display to projects page

---
+++ packages/frontend/src/pages/projects/cards/Project/StageChip.tsx
@@ -0,0 +1,33 @@
+import { Chip } from '@mui/material';
+import { api } from '@musetric/api';
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export type StageChipProps = {
+  projectInfo: api.project.Item;
+};
+
+export const StageChip: FC<StageChipProps> = (props) => {
+  const { projectInfo } = props;
+  const { t } = useTranslation();
+
+  if (projectInfo.stage === 'progress') {
+    const { separationProgress } = projectInfo;
+    const progress = separationProgress
+      ? (separationProgress * 100).toFixed(2)
+      : '--';
+    return <Chip size='small' color='info' label={`${progress}%`} />;
+  }
+
+  if (projectInfo.stage === 'pending') {
+    return (
+      <Chip
+        size='small'
+        color='warning'
+        label={t('pages.projects.stages.pending')}
+      />
+    );
+  }
+
+  return undefined;
+};
