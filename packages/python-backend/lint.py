3f18cfe01998e2360015b268c68e0ced8ac5794e
Vladlen Popelenkov <popelenkow@gmail.com>
2025-08-28 02:25:46 +0700

    Remove useless code (#232)

--- packages/python-backend/lint.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import sys
-
-script = "ruff check . --fix --no-cache && isort . && black ."
-
-sys.exit(subprocess.run(script, shell=True).returncode)
--- packages/python-backend/musetric/common/path.py
+++
@@ -1,6 +0,0 @@
-import os
-
-rootPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../")
-
-databasePath = os.path.join(rootPath, "database/index.db")
-frontendPath = os.path.join(rootPath, "dist")
