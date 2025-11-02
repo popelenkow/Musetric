f06b0a36db893c18ebf755a6830e22576e5e4955
Vladlen Popelenkov <popelenkow@gmail.com>
2025-11-02 19:55:51 +0700

    Fix backend routing to frontend (#289)

--- packages/backend/src/services/frontend.ts
+++ packages/backend/src/services/frontend.ts
@@ -24,7 +24,7 @@ export const registerFrontend = (app: FastifyInstance) => {
   });
   app.setNotFoundHandler((request, reply) => {
     if (isFrontendRequest(request)) {
-      reply.sendFile('index.html');
+      return reply.sendFile('index.html');
     }
     return reply.callNotFound();
   });
