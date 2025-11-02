266b3c652d5078889f12e10b4cf216211100d50f
Vladlen Popelenkov <popelenkow@gmail.com>
2025-11-02 19:36:50 +0700

    Move python code to musetric-toolkit repo (#288)

--- packages/backend-workers/musetricBackendWorkers/separateAudio/progress.py
+++
@@ -1,6 +0,0 @@
-import json
-
-
-def reportProgress(progress: float) -> None:
-    data = {"type": "progress", "progress": progress}
-    print(json.dumps(data), flush=True)
--- packages/backend-workers/src/common/rootPath.ts
+++
@@ -1,4 +0,0 @@
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-export const rootPath = join(dirname(fileURLToPath(import.meta.url)), '../..');
