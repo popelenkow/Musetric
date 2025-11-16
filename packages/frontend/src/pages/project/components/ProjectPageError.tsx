56d4b73d2e3e2a92664c25d1db84b2238cfaeb2c
Vladlen Popelenkov <popelenkow@gmail.com>
2025-11-17 01:41:43 +0700

    Add error handling for project preview

---
+++ packages/frontend/src/pages/project/components/ProjectPageError.tsx
@@ -0,0 +1,33 @@
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import { Stack, Typography } from '@mui/material';
+import { api } from '@musetric/api';
+import { type UseQueryResult } from '@tanstack/react-query';
+import { type FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { ProjectLayout } from './ProjectPageLayout.js';
+
+export type ProjectPageErrorProps = {
+  projectQuery: UseQueryResult<api.project.Item>;
+};
+
+export const ProjectPageError: FC<ProjectPageErrorProps> = (props) => {
+  const { projectQuery } = props;
+  const { t } = useTranslation();
+  const errorMessage = api.error.getMessage(projectQuery.error);
+
+  return (
+    <ProjectLayout isHeadingAbsolute>
+      <Stack alignItems='center' justifyContent='center' flex={1}>
+        <ErrorOutlineIcon color='error' fontSize='large' />
+        <Typography color='text.secondary'>
+          {t('common.queryView.error')}
+        </Typography>
+        {errorMessage && (
+          <Typography color='text.secondary' sx={{ wordBreak: 'break-word' }}>
+            {errorMessage}
+          </Typography>
+        )}
+      </Stack>
+    </ProjectLayout>
+  );
+};
