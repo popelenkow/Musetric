f9844732c78834e301277f4c0fe1f17cb587d234
Vladlen Popelenkov <popelenkow@gmail.com>
2025-12-15 09:27:10 +0700

    Add error handling for project preview (#300)

--- packages/frontend/src/pages/project/index.tsx
+++ packages/frontend/src/pages/project/index.tsx
@@ -5,6 +5,7 @@ import {
   subscribeToProjectStatus,
 } from '../../api/endpoints/project.js';
 import { routes } from '../../app/router/routes.js';
+import { ProjectPageError } from './components/ProjectPageError.js';
 import { ProjectPageProgress } from './ProjectPageProgress.js';
 import { ProjectView } from './View/ProjectView.js';
 
@@ -15,6 +16,10 @@ export const ProjectPage: FC = () => {
   const project = useQuery(getProjectApi(projectId));
 
   useEffect(() => subscribeToProjectStatus(queryClient), [queryClient]);
+
+  if (project.status === 'error') {
+    return <ProjectPageError projectQuery={project} />;
+  }
 
   if (project.data?.stage !== 'done') {
     return <ProjectPageProgress project={project} />;
