beb7cf1d8721e4f86496f70a15d2ff2f3dc12289
Vladlen Popelenkov <popelenkow@gmail.com>
2026-01-01 22:40:34 +0700

    Routes

--- packages/api/src/routes/index.ts
+++ packages/api/src/routes/index.ts
@@ -2,3 +2,4 @@ export * from './common/index.js';
 export * as preview from './preview.js';
 export * as project from './project.js';
 export * as sound from './sound.js';
+export * as subtitle from './subtitle.js';
