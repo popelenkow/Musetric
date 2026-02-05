46f31c069624233d3faaa60fe111ef1047893e90
Vladlen Popelenkov <popelenkow@gmail.com>
2026-02-06 03:15:53 +0700

    Support all ArrayBufferView responses for frontend (#337)

--- packages/api/src/routes/common/uint8ArraySchema.ts
+++
@@ -1,9 +0,0 @@
-import z from 'zod';
-
-export const isUint8ArraySchema = (
-  schema: unknown,
-): schema is z.ZodCustom<Uint8Array> => {
-  if (!(schema instanceof z.ZodCustom)) return false;
-  const result = schema.def.fn(new Uint8Array());
-  return !!result;
-};
